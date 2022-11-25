81a77d36f9ec5b16e3585bfc74a26bd3231ff31c
Lutfi Azis <17lutfi21@gmail.com>
2022-11-25 17:33:44 +0700

    Add 2nd attempt Swap Nodes in Pairs

--- Swap Nodes in Pairs/swap-pairs.ts
+++ Swap Nodes in Pairs/swap-pairs.ts
@@ -1,21 +1,11 @@
 import { ListNode } from '../@typings/index.js';
 
 function swapPairs(head: ListNode | null): ListNode | null {
-  if (!head) return head;
-  const origin = new ListNode(0, head);
-  let p: ListNode | null = origin,
-    q: ListNode | null = head,
-    r = q?.next ? q.next : null,
-    s = r?.next ? r.next : null;
-  while (q && r) {
-    if (p) p.next = r;
-    if (r) r.next = q;
-    q!.next = s;
-
-    p = q;
-    q = s;
-    r = s ? s.next : null;
-    s = r ? r.next : null;
+  if (!head) return null;
+  const after = head.next;
+  if (after) {
+    head.next = swapPairs(after.next);
+    after.next = head;
   }
-  return origin.next;
+  return after || head;
 }
